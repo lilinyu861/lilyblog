31dd0b464e98da330deccce08b1893330c41b88a
lily <lilinyu861@outlook.com>
2019-12-13 14:20:03 +0800

    add fragments

--- src/main/java/com/lily/myblog/web/IndexController.java
+++ src/main/java/com/lily/myblog/web/IndexController.java
@@ -1,20 +1,18 @@
 package com.lily.myblog.web;
 
-import com.lily.myblog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name){
-//        String blog = null;
-//        if (blog == null){
-//            throw new NotFoundException("博客不存在");
-//        }
-        System.out.println("--------index-------");
+    @GetMapping("/")
+    public String index(){
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        return "blog";
     }
 }
